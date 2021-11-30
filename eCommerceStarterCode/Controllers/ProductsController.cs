19878a3d89e7a60906918f17baa57f35f1b4d1f4
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-11-30 14:48:51 -0600

    update

--- eCommerceStarterCode/Controllers/ProductsController.cs
+++ eCommerceStarterCode/Controllers/ProductsController.cs
@@ -46,12 +46,7 @@ namespace eCommerceStarterCode.Controllers
 
             return Ok(searchForProducts);
 
-<<<<<<< HEAD
-            
 
-
-=======
->>>>>>> 7aba100805293de374cfd13c0135dbbc896e4c0a
         }
        
         [HttpDelete("{Id}"), Authorize]
